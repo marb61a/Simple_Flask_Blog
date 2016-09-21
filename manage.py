cea56fc8d58752843dfa72ff90899f4a3dc65c20
martin byrne <marb61a@gmail.com>
2016-09-21 12:53:03 +0000

    installed database and sqlalchemy

--- manage.py
+++ manage.py
@@ -1,5 +1,5 @@
 import os, sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '...')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask.ext.script import Manager, Server
 from flask_blog import app
