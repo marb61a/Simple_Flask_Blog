78445b27deb2196631a8f8ba7a2b41dad6d7045f
martin byrne <marb61a@gmail.com>
2016-10-10 10:15:45 +0000

    updated blog form

--- blog/form.py
+++ blog/form.py
@@ -13,3 +13,15 @@ class SetupForm(Form):
     username = StringField('Username', [validators.Required(), validators.Length(min=4, max=25)])
     password = PasswordField('New Password', [validators.Required(), validators.EqualTo('confirm', message='Passwords must match'), validators.Length(min=4, max=80)])
     confirm = PasswordField('Repeat Password')
+    
+def categories():
+    return Category.query
+    
+class PostForm(Form):
+    image = FileField('Image', validators=[
+        FileAllowed(['jpg', 'png'], 'Images only!')    
+    ])
+    title = StringField('Title', [validators.Required(), validators.Length(max=80)])
+    body = TextAreaField('Content', validators=[validators.Required()])
+    category = QuerySelectField('Category', query_factory=categories, allow_blank=True)
+    new_category = StringField('New Category')
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,15 @@
 from flask_blog import app
+from flask import render_template, redirect, flash, url_for, session, request
+from blog.form import SetupForm, PostForm
+from flask_blog import db, uploaded_images
+from author.models import Author
+from blog.models import Blog, Post, Category
+from author.decorators import login_required, author_required
+import bcrypt
+from slugify import slugify
+from flask_uploads import UploadNotAllowed
+
+POSTS_PER_PAGE = 5
 
 @app.route('/')
 @app.route('/index')
