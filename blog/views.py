f8d92aa4ebdd40532a195e76a124e9ef0b11f59d
martin byrne <marb61a@gmail.com>
2016-10-13 09:44:48 +0000

    updated blog views with index, admin and setup route bcrypt

--- blog/views.py
+++ blog/views.py
@@ -13,6 +13,34 @@ POSTS_PER_PAGE = 5
 
 @app.route('/')
 @app.route('/index')
-
-def index():
-    return "Hello World"
+@app.route('/index/<int:page>')
+def index(page=1):
+    blog = Blog.query.first()
+    posts = Post.query.filter_by(live=True).order_by(Post.publish_date.desc()).paginate(page, POSTS_PER_PAGE, False)
+    return render_template('blog/index.html', blog=blog, posts=posts)
+    
+@app.route('/admin')
+@app.route('/admin/<int:page>')
+@login_required
+@author_required
+def admin(page=1):
+    posts = Post.query.order_by(Post.publish_date.desc()).paginate(page, POSTS_PER_PAGE, False)
+    return render_template('blog/admin.html', posts=posts)
+    
+@app.route('/setup', methods=('GET, POST'))
+def setup():
+    blogs = Blog.query.count()
+    if blogs():
+        return redirect(url_for('admin'))
+    form = SetupForm()
+    if form.validate_on_submit():
+        salt = bcrypt.gensalt()
+        hashed_password = bcrypt.hashpw(form.password.data, salt)
+        author = Author(
+            form.fullname.data,
+            form.email.data,
+            form.username.data,
+            hashed_password,
+            True
+        )
+    
