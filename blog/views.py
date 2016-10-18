85a84dd202bac784a8bab2c5c47ab2cd17b8b84d
martin byrne <marb61a@gmail.com>
2016-10-18 10:15:10 +0000

    updated blog views

--- blog/views.py
+++ blog/views.py
@@ -82,7 +82,7 @@ def post():
         blog = Blog.query.first()
         author = Author.query.filter_by(username=session['username']).first()
         title = form.title.data
-        form = form.body.data
+        body = form.body.data
         slug = slugify(title)
         post = Post(blog, author, title, body, category, filename, slug)
         db.session.add(post)
@@ -90,5 +90,44 @@ def post():
         return redirect(url_for('article', slug=slug))
     return render_template('blog/post.html', form=form, action="new")
 
-
+@app.route('/article/<slug>')
+def article(slug):
+    post = Post.query.filter_by(slug=slug).first_or_404()
+    return render_template('blog/article.html', post=post)
+    
+@app.route('/edit/<int:post_id>', methods=('GET', 'POST'))
+@author_required
+def edit(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    form = PostForm(obj=post)
+    if form.validate_on_submit():
+        original_image = post.image
+        form.populate_obj(post)
+        if form.image.has_file():
+            image = request.files.get('image')
+            try:
+                filename = uploaded_images.save(image)
+            except:
+                flash("The image was not uploaded")
+            if filename:
+                post.image = filename
+        else:
+            post.image = original_image
+        if form.new_category.data:
+            new_category = Category(form.new_category.data)
+            db.session.add(new_category)
+            db.session.flush()
+            post.category = new_category
+        db.session.commit()
+        return redirect(url_for('article', slug=post.slug))
+    return render_template('blog/post.html', form=form, post=post, action="edit")
+    
+@app.route('/delete/<int:post_id>')
+@author_required
+def delete(post_id):
+    post = Post.query.filter_by(id=post_id).first_or_404()
+    post.live = False
+    db.session.commit()
+    flash('Article Deleted')
+    return redirect('/admin')
     
